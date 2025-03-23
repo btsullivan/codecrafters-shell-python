7b3c11ab4cb3702ce522c6e3f23939a00cd4cd1b
Brett Sullivan <btsullivan@tylerssoftware.com>
2025-03-23 17:31:03 -0500

    Adding type command fixed invalid commands for type

--- app/main.py
+++ app/main.py
@@ -12,10 +12,11 @@ def main():
         if command == "exit 0":
             x = False
         elif "type" in command:
-            #command = command.replace("type","").strip()
-            print(f"{command[5:]} is a shell builtin")
+            if command[5:] == "type" or command[5:] == "echo" or command[5:] == "exit":
+                print(f"{command[5:]} is a shell builtin")
+            else:
+                print(f"{command}: command not found")
         elif "echo" in command:
-            #command = command.replace("echo","").strip()
             print(f"{command[5:]}")
         else:
             print(f"{command}: command not found")
