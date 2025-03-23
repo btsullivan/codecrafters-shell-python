35a1e5bb6abf54bead0008fe6fa3401b49d84d22
Brett Sullivan <btsullivan@tylerssoftware.com>
2025-03-23 16:19:55 -0500

    REPL Stage

--- app/main.py
+++ app/main.py
@@ -2,13 +2,18 @@ import sys
 
 
 def main():
-    while True:
+    x = True
+    while x == True:
         # Uncomment this block to pass the first stage
         sys.stdout.write("$ ")
 
         # Wait for user input
         command = input()
-        print(f"{command}: command not found")
+        if command == "exit 0":
+            x = False
+        elif command != "exit 0":
+            print(f"{command}: command not found")
+
 
 
 if __name__ == "__main__":
